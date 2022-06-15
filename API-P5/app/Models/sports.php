36a87e5d8700913150bd595c838ff71ae35de0c9
ps220812 <ps220812@summacollege.nl>
2022-06-15 09:20:55 +0200

    models filled in

--- API-P5/app/Models/sports.php
+++ API-P5/app/Models/sports.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class sports extends Model
 {
     use HasFactory;
+    protected $table = 'sports';
+    protected $fillable = [
+        'id',
+        'name',
+    ];
 }
