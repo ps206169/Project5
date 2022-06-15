f47c64f3f30d9ef509938c93d62dfcc8cf5a4ed1
ps220812 <ps220812@summacollege.nl>
2022-06-15 09:12:46 +0200

    finished sportsSeeder

--- API-P5/database/seeders/SportsSeeder.php
+++ API-P5/database/seeders/SportsSeeder.php
@@ -16,14 +16,16 @@ class SportsSeeder extends Seeder
     public function run()
     {
         $sports = [
-            ['id' => 1, 'name' => 'squat'],
-            ['id' => 2, 'name' => 'push-up'],
-            ['id' => 3, 'name' => 'dip'],
-            ['id' => 3, 'name' => 'dip'],
-            ['id' => 3, 'name' => 'dip'],
-            ['id' => 3, 'name' => 'dip'],
-            ['id' => 3, 'name' => 'dip'],
-            ['id' => 3, 'name' => 'dip'],
+            ['id' => 1, 'name' => 'Squat'],
+            ['id' => 2, 'name' => 'Push-up'],
+            ['id' => 3, 'name' => 'Dip'],
+            ['id' => 4, 'name' => 'Plank'],
+            ['id' => 5, 'name' => 'Paardentrap'],
+            ['id' => 6, 'name' => 'Mountain climber'],
+            ['id' => 7, 'name' => 'Burpee'],
+            ['id' => 8, 'name' => 'Lunge'],
+            ['id' => 9, 'name' => 'Wall sit'],
+            ['id' => 10, 'name' => 'Crunch'],
         ];
 
         foreach($sports as $sport){
