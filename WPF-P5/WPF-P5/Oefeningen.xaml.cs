d1d8756190289b21cc4089865a608fcad8f5e75f
ps222908 <74544154+ps222908@users.noreply.github.com>
2022-06-22 10:42:44 +0200

    setupwpf done

---
+++ WPF-P5/WPF-P5/Oefeningen.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WPF_P5.models;
+
+namespace WPF_P5
+{
+    /// <summary>
+    /// Interaction logic for Oefeningen.xaml
+    /// </summary>
+    public partial class Oefeningen : Window
+    {
+
+        private ObservableCollection<SportData> sportDataList;
+
+        public ObservableCollection<SportData> SportDataList
+        {
+            get { return sportDataList; }
+            set { sportDataList = value; }
+        }
+
+        public Oefeningen()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void Wijzig_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Verwijder_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
