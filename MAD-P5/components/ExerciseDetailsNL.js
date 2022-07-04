cfea81ba44d4a0bb08fd53b01ac9ce143c85104a
ps220812 <ps220812@summacollege.nl>
2022-07-04 11:49:03 +0200

    API Get werkt

--- MAD-P5/components/ExerciseDetailsNL.js
+++ MAD-P5/components/ExerciseDetailsNL.js
@@ -1,68 +1,51 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React, { useState, useEffect } from 'react'
+import { ActivityIndicator } from 'react-native-paper';
 
-const ExerciseDetailsNL = () => {
+const ExerciseDetailsNL = (props) => {
     const apiSelectedExercise = `https://eindopdrachtsummamove.nl/api/oefeningen/${props.route.params.id}`;
-    const [isAvailable, setAvailable] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
     const [data, setData] = useState(null);
 
-    useEffect(() => { GetData(); }, []);
-
-    const HandleError = (error) => {
-        console.log(error);
-        setData(
-            {
-                results: [{
-                    name: 'Er is een fout opgetreden. Start de applicatie opnieuw op. '
-                        + 'Blijft het probleem optreden, waarschuw dan de service desk.'
-                }]
-            }
-        )
+    const GetSelectedExercise = async () => {
+        try {
+            const response = await fetch(apiSelectedExercise);
+            const json = await response.json();
+            setData(json);
+        } catch (error) {
+            console.error(error);
+        }
+        finally {
+            setIsLoading(false);
+        }
     }
 
-    const GetData = () => {
-        fetch(apiSelectedExercise)
-            .then((response) => response.json())
-            .then((data) => setData(data))
-            .catch((error) => HandleError(error))
-            .finally(() => setAvailable(true));
-    }
-
-    const RenderData = ({ item, index }) => {
-        const id = item.id;
-        return (
-            <View>
-                <Text>{item.instructionNL}</Text>
-            </View>
-        )
-    }
+    useEffect(() => { GetSelectedExercise(); }, []);
 
     return (
         <View style={styles.container}>
-            {isAvailable
-                ?
-                (
-                    <View style={styles.container}>
-                        <FlatList
-                            data={data.results}
-                            renderItem={RenderData}
-                            keyExtractor={(item, index) => item.id + item.nameNL + index}
-                        />
-                    </View>
-                )
-                :
-                (
-                    <View style={styles.container}>
-                        <Text>Er is een fout opgetreden. Start de applicatie opnieuw op. '
-                                + 'Blijft het probleem optreden, waarschuw dan de service desk.'
-                        </Text>
-                    </View>
-                )
-            }
+            {isLoading ? <ActivityIndicator/>: (
+                <Text style={styles.item}>{data.nameNL}</Text>
+            )}
+            {isLoading ? <ActivityIndicator/>: (
+                <Text style={styles.item}>{data.instructionNL}</Text>
+            )}
         </View>
-    )
+    );
 }
 
 export default ExerciseDetailsNL;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    header: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+})
